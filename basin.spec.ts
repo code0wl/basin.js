a6ac0028f36b8c4c569b6768854bc46d487c6aff
code0wl <o.lodriguez@gmail.com>
2018-05-13 07:50:34 +0200

    readme update

--- basin.spec.ts
+++ basin.spec.ts
@@ -2,11 +2,11 @@ import { Basin } from './basin';
 
 test('should work with numbers', () => {
 
-    const calculateTotal = initial =>
+    const calculateTotal = (initial: number) =>
         Basin(initial)
-            .tap(s => s * .21) // add tax
-            .tap(s => s + 12) // shipping costs
-            .drop(total => initial + total);
+            .tap((s: number) => s * .21) // add tax
+            .tap((s: number) => s + 12) // shipping costs
+            .drop((total: number) => initial + total);
 
     const result = calculateTotal(100);
 
@@ -16,12 +16,12 @@ test('should work with numbers', () => {
 
 test('should work with strings', () => {
 
-    const combineAllTheThings = initial =>
+    const combineAllTheThings = (initial: string) =>
         Basin(initial)
-            .drop(s => s + ' World') // add tax
+            .drop((s: string) => s + ' World')
 
     const result = combineAllTheThings('Hello');
 
-    expect(result).toBe('Hello World'); //?
+    expect(result).toBe('Hello World');
 
 });
